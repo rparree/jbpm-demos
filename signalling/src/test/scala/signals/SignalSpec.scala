ea8ed687ceb2390b9f197f2c19432bc1e7f49f11
Raphael Parree <rparree@edc4it.com>
2015-09-22 17:05:56 +0200

    Improved the signal demo

--- signalling/src/test/scala/signals/SignalSpec.scala
+++ signalling/src/test/scala/signals/SignalSpec.scala
@@ -8,10 +8,13 @@ import org.kie.api.runtime.process.ProcessInstance
 import org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED
 import org.specs2.mutable.Specification
 import scala.collection.JavaConversions._
-
-
+import scala.concurrent.duration._
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class SignalSpec extends Specification with KieTestContext {
+
+  sequential
 
   override val config = KBaseSession("signalDemo", "mysession")
 
@@ -20,6 +23,14 @@ class SignalSpec extends Specification with KieTestContext {
     val noParams: Map[String, AnyRef] = Map()
     val fooProcess = s.startProcess("signals.FooProcess",noParams )
     val mainProcess = s.startProcess("signals.MainSignallingProcess", params)
+    ok("smoke")
+
+
+  }
+
+  "foo timeout" >> { s: KieSession =>
+    val noParams: Map[String, AnyRef] = Map()
+    val fooProcess = s.startProcess("signals.FooProcess",noParams )
     ok("smoke")
 
 
