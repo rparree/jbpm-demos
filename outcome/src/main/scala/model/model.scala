2e2b9e5788dedc3e8f8076df3126250537783469
Raphael Parree <rparree@edc4it.com>
2015-09-22 15:42:37 +0200

    Added test and Simplified & Cleanup for the outcome demo

--- outcome/src/main/scala/model/model.scala
+++ outcome/src/main/scala/model/model.scala
@@ -1,44 +1,12 @@
 package model
 
-import java.util.Date
-
-import model.RejectionLevel.RejectionLevel
-
-
 import scala.beans.BeanProperty
 
-
-case class Driver(@BeanProperty var driverName: String,
-                  @BeanProperty var age: Integer,
-                  @BeanProperty var ssn: String,
-                  @BeanProperty var dlNumber: String,
-                  @BeanProperty var numberOfAccidents: Integer,
-                  @BeanProperty var numberOfTickets: Integer,
-                  @BeanProperty var creditScore: Integer
-                 ) {
-
+case class Policy(@BeanProperty var risk: Integer) {
+  @BeanProperty var price = 0
 }
 
-case class Policy(@BeanProperty var requestDate: Date,
-                  @BeanProperty var policyType: String,
-                  @BeanProperty var vehicleYear: Integer,
-                  @BeanProperty var price: Integer,
-                  @BeanProperty var priceDiscount: Integer,
-                  @BeanProperty var driver: Driver,
+case class Rejection(@BeanProperty var reason: String)
 
-                  @BeanProperty var risk: Integer) {
-
-
-}
-
-case class Rejection(@BeanProperty var reason: String,
-                     @BeanProperty var level: RejectionLevel) {
-
-}
-
-object RejectionLevel extends Enumeration {
-  type RejectionLevel = Value
-  val NONE, ERROR, FATAL = Value
-}
 
 
---
+++ outcome/src/test/scala/demo/OutComeTest.scala
@@ -0,0 +1,44 @@
+package demo
+
+import java.util.Date
+
+import model.Policy
+import org.jbpm.test.JbpmJUnitBaseTestCase
+import org.junit.{Before, Test}
+import org.kie.api.io.ResourceType
+import org.kie.api.runtime.KieSession
+import org.kie.api.runtime.process.ProcessInstance
+
+import scala.collection.JavaConversions._
+
+class OutComeTest extends JbpmJUnitBaseTestCase(true,true) {
+
+  @Before
+  def setupKie :Unit = createRuntimeManager(Map("policy/risk.bpmn2"->ResourceType.BPMN2,"policy/policy.drl"->ResourceType.DRL))
+
+  @Test
+  def highRiskTest() : Unit = {
+    val session: KieSession = getRuntimeEngine.getKieSession
+
+    val params : Map[String,AnyRef] = Map("request" -> Policy(600) )
+    val pi: ProcessInstance = session.startProcess("policy.Risk", params)
+    session.fireAllRules
+
+    assertNodeTriggered(pi.getId,"High Risk")
+    assertProcessInstanceAborted(pi.getId)
+  }
+
+  @Test
+  def lowRiskTest() : Unit = {
+    val session: KieSession = getRuntimeEngine.getKieSession
+
+    val params : Map[String,AnyRef] = Map("request" -> Policy(100) )
+    val pi: ProcessInstance = session.startProcess("policy.Risk", params)
+    session.fireAllRules
+
+    assertNodeTriggered(pi.getId,"End")
+    assertProcessInstanceCompleted(pi.getId)
+  }
+
+
+}
