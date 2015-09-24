8a97cc7cd9469bed87191bf092fde6f736154476
Raphael Parree <rparree@edc4it.com>
2015-09-24 11:19:45 +0200

    Fixed the KieTestContext
    It was closing the RTM forEach, so must then recreate in order to run multiple tests

--- util/src/main/scala/demo.util/KieTestContext.scala
+++ util/src/main/scala/demo.util/KieTestContext.scala
@@ -3,15 +3,16 @@ package demo.util
 import org.drools.core.spi.ProcessContext
 import org.jbpm.bpmn2.handler.ServiceTaskHandler
 import org.kie.api.runtime.{KieRuntime, KieSession}
-import org.kie.api.runtime.manager.RuntimeEngine
+import org.kie.api.runtime.manager.{RuntimeManager, RuntimeManagerFactory, RuntimeEngine}
 import org.kie.internal.runtime.manager.context.EmptyContext
 import org.specs2.execute.{Result, AsResult}
 import org.specs2.specification.ForEach
 
 
-trait KieTestContext extends ForEach[KieSession]  with DefaultClasspathRuntimeEnvironment with SingletonRuntimeManager with H2 {
+trait KieTestContext extends ForEach[KieSession]  with DefaultClasspathRuntimeEnvironment with H2 {
 
   def foreach[R : AsResult](f : KieSession => R) : Result = {
+    val runtimeManager: RuntimeManager = RuntimeManagerFactory.Factory.get.newSingletonRuntimeManager(environment)
     val runtimeEngine: RuntimeEngine = runtimeManager.getRuntimeEngine(EmptyContext.get)
     val session: KieSession = runtimeEngine.getKieSession
     session.getWorkItemManager().registerWorkItemHandler("Service Task", new ServiceTaskHandler())
